2147dbb767a3977d3a0ce4702ffc10fc83af0820
DadoFPS <101755502+DadoFPS@users.noreply.github.com>
2022-04-04 19:58:23 -0300

    adicionamos os obstaculos e a pontuacao

--- sketch.js
+++ sketch.js
@@ -1,38 +1,143 @@
-var hex;
-var animation1;
-var solo;
-var animation2;
+var trex, trex_running, trex_collided;
+var ground, invisibleGround, groundImage;
 
-function preload () {
+var cloud, cloudsGroup, cloudImage;
 
-  animation1 = loadAnimation ("trex1.png","trex3.png","trex4.png");
-  animation2 = loadImage ("ground1.png");
+var obestaculo1,obestaculo2,obestaculo3,obestaculo4,obestaculo5,obestaculo6;
+
+var obstaculo;
+
+var pontos = 0;
+
+
+var newImage;
+
+function preload(){
+  trex_running = loadAnimation("trex1.png","trex3.png","trex4.png");
+  trex_collided = loadAnimation("trex_collided.png");
   
+  groundImage = loadImage("ground2.png");
   
-  }
+  cloudImage = loadImage("cloud.png");
 
-function setup(){
-  createCanvas(600,200);
-  hex = createSprite (50,110,20,20);
-  hex.addAnimation ("correndo",animation1);
-  hex.scale = 0.5;
-  solo = createSprite (200,180,400,20);
-  solo.addImage ("chao",animation2);
+  obestaculo1 = loadImage ("obstacle1.png");
+  obestaculo2 = loadImage ("obstacle2.png");
+  obestaculo3 = loadImage ("obstacle3.png");
+  obestaculo4 = loadImage ("obstacle4.png");
+  obestaculo5 = loadImage ("obstacle5.png");
+  obestaculo6 = loadImage ("obstacle6.png");
+ 
 }
 
-function draw(){
+function setup() {
+  createCanvas(600, 200);
+
+  trex = createSprite(50,160,20,50);
+  trex.addAnimation("running", trex_running);
+  
+  trex.scale = 0.5;
+  
+  ground = createSprite(200,180,400,20);
+  ground.addImage("ground",groundImage);
+  ground.x = ground.width /2;
+  ground.velocityX = -4;
+
+
+  
+  invisibleGround = createSprite(200,190,400,10);
+  invisibleGround.visible = false;
+  
+  
+}
+
+function draw() {
   background("white");
-  drawSprites();
-  pular ();
-  hex.collide (solo);
-}
-function pular () {
+  
+  pontos = pontos + Math.round (frameCount /60)
 
-  if (keyDown ("up")) {
-    hex.velocityY = -10;
+  text ("pontos:" + pontos,500,50);
+  
+  if(keyDown("up") && trex.y>=161) {
+    trex.velocityY = -10;
+  }
+  
+  trex.velocityY = trex.velocityY + 0.8
+  
+  if (ground.x < 0){
+    ground.x = ground.width/2;
+  }
+  
+  trex.collide(invisibleGround);
+  
+  
+  spawnClouds();
+  spawnObs ();
+  
+  drawSprites();
+}
+
+function spawnClouds() {
+  
+  if (frameCount % 60 === 0) {
+    cloud = createSprite(600,100,40,10);
+    cloud.addImage(cloudImage)
+    cloud.y = Math.round(random(10,60))
+    cloud.scale = 0.4;
+    cloud.velocityX = -3;
+    
+   
+    cloud.depth = trex.depth
+    trex.depth = trex.depth + 1;
+    cloud.lifetime = 220;
+    }
+}
+
+
+function spawnObs () {
+
+if (frameCount % 100 === 0) {
+
+  obstaculo = createSprite (605,165,10,40);
+
+  switch (Math.round (random (1,6))) {
+
+    case 1:
+      obstaculo.addImage (obestaculo1);
+      break
+
+    case 2:
+      obstaculo.addImage (obestaculo2);
+      break
+
+    case 3:
+      obstaculo.addImage (obestaculo3);
+      break
+
+    case 4:
+      obstaculo.addImage (obestaculo4);
+      break
+
+    case 5:
+      obstaculo.addImage (obestaculo5);
+      break
+
+    case 6:
+      obstaculo.addImage (obestaculo6);
+      break
+
+      
+
 
   }
-  	hex.velocityY += 0.8;
+
+  obstaculo.velocityX= -4;
+  obstaculo.scale = 0.4;
+  obstaculo.lifetime = 220;
+
+}
+
+
 
 
 }
+
